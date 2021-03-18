cd1671ec9f7ba8b8574302684ee4a973d1286651
Matt-Harvill <m.harvill23@gmail.com>
2021-03-18 16:18:00 -0500

    commit with new server connect scene. minor updates

--- src/controller/ServerConnectController.java
+++ src/controller/ServerConnectController.java
@@ -12,30 +12,22 @@ import java.io.IOException;
 
 public class ServerConnectController {
 
-    private static String ipAddressOfHost, portOfHost;
-
     @FXML private TextField ipAddressBox, portNumBox;
     @FXML private Button submitButton;
     @FXML private Text errorMessage;
 
-    public void initialize() {
-        errorMessage.setVisible(false);
-    }
-
     public void onLoginSubmit(ActionEvent actionEvent) {
 
-        ipAddressOfHost = ipAddressBox.getText();
-        portOfHost = portNumBox.getText();
+
+        String ipAddressOfHost = ipAddressBox.getText();
+        String portOfHost = portNumBox.getText();
 
         try {
             new ClientSideConnection(ipAddressOfHost,portOfHost);
             UserInterfaceHub.switchToGameScene();
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             errorMessage.setText("Incorrect Host IP and/or Port, Please Try Again");
-        } catch (NumberFormatException num) {
-            errorMessage.setText("Please Enter a Valid Port Number");
         }
-        errorMessage.setVisible(true);
         ipAddressBox.setText(null);
         portNumBox.setText(null);
     }
