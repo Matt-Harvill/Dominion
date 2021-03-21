f32fe00a27a15351e2acb039b110b1bb7137370c
Matt-Harvill <m.harvill23@gmail.com>
2021-03-21 00:37:22 -0500

    reimplemented all imageviews with rectangles so that I can easily apply styles to borders and such.

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -2,44 +2,71 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.control.*;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
 
-    @FXML private Label actionCardInSupplyNum1;
-    @FXML private BorderPane mainBorderPane;
-    @FXML private StackPane mainStackPane;
+    //----------------CSS Styles for Cards---------------//
+    private final String greenCardGlowStyle = "-fx-stroke-width: 3; -fx-stroke: #54ff54;";
+    private final String redCardGlowStyle = "-fx-stroke-width: 3; -fx-stroke: #ff0101;";
+
+    //---------------Chat Log and Game Log--------------//
     @FXML private TextArea chatLog, gameLog;
     @FXML private TextField chatType;
-
-    @FXML private ImageView cardInHand1,cardInHand2,cardInHand3,cardInHand4,cardInHand5,cardInHand6,cardInHand7,cardInHand8,cardInHand9,cardInHand10,cardInHand11;
-
     private List<String> chatDisplayStrings, gameDisplayStrings;
-    private ImageView[] cardsInHand;
+
+    //---------------Cards In Hand---------------//
+    @FXML private Rectangle cardInHand1,cardInHand2,cardInHand3,cardInHand4,cardInHand5,cardInHand6,cardInHand7,cardInHand8,cardInHand9,cardInHand10,cardInHand11;
+    private Rectangle[] cardsInHand;
+
+    //---------------ActionCards In Supply---------------//
+    @FXML private Rectangle actionCardInSupply1,actionCardInSupply2,actionCardInSupply3,actionCardInSupply4,actionCardInSupply5,
+            actionCardInSupply6,actionCardInSupply7,actionCardInSupply8,actionCardInSupply9,actionCardInSupply10;
+    private Rectangle[] actionCardsInSupply;
+
+    //---------------Opponent Played Cards---------------//
+    @FXML private Rectangle oppCardPlayed1,oppCardPlayed2,oppCardPlayed3,oppCardPlayed4,oppCardPlayed5,oppCardPlayed6,
+            oppCardPlayed7,oppCardPlayed8,oppCardPlayed9,oppCardPlayed10,oppCardPlayed11;
+    private Rectangle[] oppCardsPlayed;
 
     public void initialize() {
         chatDisplayStrings = new ArrayList<>();
         gameDisplayStrings = new ArrayList<>();
-        cardsInHand = new ImageView[]{cardInHand1, cardInHand2, cardInHand3, cardInHand4, cardInHand5, cardInHand6, cardInHand7, cardInHand8, cardInHand9, cardInHand10, cardInHand11};
+
+        //--------------Initialize all cardsInHand to coppers-----------------//
+        cardsInHand = new Rectangle[]{cardInHand1, cardInHand2, cardInHand3, cardInHand4, cardInHand5, cardInHand6, cardInHand7, cardInHand8, cardInHand9, cardInHand10, cardInHand11};
+        ImagePattern imagePattern1 = new ImagePattern(new Image(new File("src/resources/Copper.jpg").toURI().toString()));
         for(int i=0; i<cardsInHand.length; i++) {
             cardsInHand[i].setViewOrder(cardsInHand.length-i);
+            cardsInHand[i].setFill(imagePattern1);
         }
+
+        //--------------Initialize all ActionCardsInSupply to Villages-----------------//
+        actionCardsInSupply = new Rectangle[]{actionCardInSupply1,actionCardInSupply2,actionCardInSupply3,actionCardInSupply4,actionCardInSupply5,
+                actionCardInSupply6,actionCardInSupply7,actionCardInSupply8,actionCardInSupply9,actionCardInSupply10};
+        ImagePattern imagePattern2 = new ImagePattern(new Image(new File("src/resources/Village.jpg").toURI().toString()));
+        for (Rectangle rectangle : actionCardsInSupply) {
+            rectangle.setFill(imagePattern2);
+        }
+
+        //--------------Initialize all ActionCardsInSupply to Villages-----------------//
+        oppCardsPlayed = new Rectangle[]{oppCardPlayed1,oppCardPlayed2,oppCardPlayed3,oppCardPlayed4,oppCardPlayed5,oppCardPlayed6,
+                oppCardPlayed7,oppCardPlayed8,oppCardPlayed9,oppCardPlayed10,oppCardPlayed11};
+//        ImagePattern imagePattern3 = new ImagePattern(new Image(new File("src/resources/Duchy.jpg").toURI().toString()));
+        for (Rectangle rectangle : oppCardsPlayed) {
+//            rectangle.setFill(imagePattern3);
+            rectangle.setVisible(false);
+        }
+
     }
 
     //-------------------Internal Updates------------------------//
@@ -51,20 +78,26 @@ public class GameController {
     }
 
     public void cardInHandClicked(MouseEvent mouseEvent) {
-        ImageView cardClicked = (ImageView) mouseEvent.getSource();
-        for(ImageView card: cardsInHand) {
-            if(card.equals(cardClicked)) System.out.println(card.toString() + " was clicked");
+        Rectangle cardClicked = (Rectangle) mouseEvent.getSource();
+        for(Rectangle card: cardsInHand) {
+            if(card.equals(cardClicked)) {
+                System.out.println(card.toString() + " was clicked");
+                card.setStyle(greenCardGlowStyle);
+            }
         }
 
     }
-    public void mouseOverCardInHand(MouseEvent mouseEvent) throws FileNotFoundException {
-        ImageView cardClicked = (ImageView) mouseEvent.getSource();
-        for(ImageView card: cardsInHand) {
-            if(card.equals(cardClicked)) card.setViewOrder(0);
+    public void mouseOverCardInHand(MouseEvent mouseEvent) {
+        Rectangle cardClicked = (Rectangle) mouseEvent.getSource();
+        for(Rectangle card: cardsInHand) {
+            if(card.equals(cardClicked)) {
+                card.setViewOrder(0);
+                card.setStyle(redCardGlowStyle);
+            }
         }
     }
     public void mouseExitedCardInHand(MouseEvent mouseEvent) {
-        ImageView cardClicked = (ImageView) mouseEvent.getSource();
+        Rectangle cardClicked = (Rectangle) mouseEvent.getSource();
         for(int i=0; i<cardsInHand.length; i++) {
             if(cardsInHand[i].equals(cardClicked)) cardsInHand[i].setViewOrder(cardsInHand.length-i);
         }
